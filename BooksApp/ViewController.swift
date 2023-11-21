0397afb7dfd36e55b00e830a94e7f867e4b246b5
lukyanenk7 <vladimir.lukyanenko@trident-media.agency>
2023-11-21 08:54:02 +0200

    Initial Commit

---
+++ BooksApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BooksApp
+//
+//  Created by Vladimir Lukyanenko on 21.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
