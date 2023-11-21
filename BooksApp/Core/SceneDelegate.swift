7054a5f9aedd8e1e84ade4d75d0c93d491edf718
lukyanenk7 <vladimir.lukyanenko@trident-media.agency>
2023-11-21 09:14:17 +0200

    first commit

---
+++ BooksApp/Core/SceneDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  SceneDelegate.swift
+//  BooksApp
+//
+//  Created by Vladimir Lukyanenko on 21.11.2023.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let vc = UIHostingController(rootView: MainPage())
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
+    }
+
+
+
+
+}
+
